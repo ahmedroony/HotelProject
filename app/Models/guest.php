049bd8562a82d46ccd762fbff00b0cd5005b824c
Malaz-Hatim <malazhatim79@gmail.com>
2025-09-18 22:22:16 +0300

    giest model

--- app/Models/guest.php
+++ app/Models/guest.php
@@ -4,8 +4,26 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;//don`t forget this
 use Illuminate\Database\Eloquent\Model;
 
+
 class Guest extends Model
 {
-    use HasFactory;//and this
-    protected $fillable = ["first_name","last_name","email","phone","date_of_birth"];
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     */
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'email',
+        'phone',
+        'date_of_birth',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     */
+    protected $casts = [
+        'date_of_birth' => 'date',
+    ];
 }
